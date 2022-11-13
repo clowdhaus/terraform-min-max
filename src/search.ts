3293c04ab03c1cea6bfbb49536877f156672c001
Bryant Biggs <bryantbiggs@gmail.com>
2022-11-13 10:54:55 -0500

    chore: Remove test console.log and add debug output that can be enabled

--- src/search.ts
+++ src/search.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import * as findInFiles from 'find-in-files';
 
 const regExprRequiredVersion = /(?<=(required_version.=.)).*/;
@@ -5,17 +6,18 @@ const regExprWrappers = /wrappers/;
 
 export async function versionConstraintSearch(dir: string): Promise<string> {
   const files = await findInFiles.find('required_versions*s*', dir, '.tf$');
+  core.debug(`files: ${files}`);
 
-  console.log(files);
-
-  const key = Object.keys(files)
+  const filteredResults = Object.keys(files)
     .sort((a, b) => a.length - b.length)
-    .filter(word => !regExprWrappers.test(word))[0];
-  const line = files[key].line;
+    .filter(word => !regExprWrappers.test(word));
+  core.debug(`filteredResults: ${filteredResults}`);
+  const line = files[filteredResults[0]].line;
 
   if (line) {
     const extractResults = regExprRequiredVersion.exec(line);
     const res = extractResults ? extractResults[0] : '';
+    core.debug(`Result: ${res}`);
     return res;
   }
 
